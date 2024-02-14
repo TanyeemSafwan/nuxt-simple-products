84b5fc150b515073e65407a50977e952fab1d92c
TanyeemSafwan <tanyeemsafwan123@gmail.com>
2024-02-14 08:00:13 +0600

    added defaults pages with layouts

---
+++ pages/products/[id].vue
@@ -0,0 +1,17 @@
+<script setup>
+  const { id } = useRoute().params;
+  definePageMeta({
+  layout: 'products'
+  });
+</script>
+
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum suscipit libero dolore, numquam a eligendi tempora sapiente sunt ex architecto saepe eaque natus placeat, impedit optio? Inventore incidunt nulla ducimus?</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
